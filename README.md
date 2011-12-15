ff9fd5a0b957841ad11618c897ab986c43e0621f
Drew Pirrone-Brusse <drew@asz.vg>
2011-12-14 19:51:44 -0500

    First commit for the gh-page

---
+++ README.md
@@ -0,0 +1,42 @@
+# Game\_Of\_Life.js #
+How do I teach myself HTML5, CSS3 and Javascript?  
+Yeah. This is how.
+
+-----
+
+### Introduction ###
+This is my implementation of John Conway's [Game of Life](http://en.wikipedia.org/wiki/Conway's_Game_of_Life). It is the first project I've embarked on using HTML5, [Sass](http://sass-lang.com/) and [CoffeScript](http://coffeescript.org/) for building a web-site front-end. (Actually... I guess it's more of a web-app?)
+
+### Credits & How I Done It ###
+To start; [HTML5](http://www.quackit.com/html_5/tags/), [CSS3](http://www.w3schools.com/cssref/default.asp), and [Javascript](http://www.ecmascript.org/).  
+In truth, I didn't much use the ECMAScript standard to figure out JavaScript. Rather, I found [Douglas Crockford's](http://yuiblog.com/crockford/) video lectures on JavaScript to be a wonderful resource. Everyone should watch that first lecture. Really. It's not even about JavaScript.
+
+After building a proficiency with those lectures, a _lot_ of playing around, and this wonderful [garden of JavaScript-gotchas](http://bonsaiden.github.com/JavaScript-Garden/), I abandoned JavaScript in favor of [CoffeeScript](http://coffeescript.org/). If you have any opinion on JavaScript&mdash;positive or negative&mdash;go check out CoffeeScript. I recommend starting with [The Little Book on CoffeeScript](http://arcturo.github.com/library/coffeescript/).
+
+Similarly, after spending a great deal of time wrestling with CSS (Seriously? **Still** [no variables](http://s3.amazonaws.com/kym-assets/photos/images/original/000/000/578/1234931504682.jpg)?!), I gave up on that outmoded beast in favor of [Sass](http://sass-lang.com/). The added compilation step takes a bit longer than I'd like, but as a trade-off for variables, mix-ins and a bunch of super-handy functions, I'll take it.
+
+On the topic of compilation, I found the [Watchr](https://github.com/mynyml/watchr) package to greatly improve my workflow.  
+Automatic compilation in one simple Ruby script? Yes please.
+
+In the actual production of this Game_Of_Life.js, I first have to attribute [JQuery](http://jquery.com/) for... well, for being JQuery. I mean, there are alternatives, but... What more is there to say? It's JQuery.
+
+I used a few pieces from [JQuery UI](http://jqueryui.com/) in the creation of the control pane. I'm still really iffy on using JQuery UI in production code&mdash;the theme-roller just doesn't cut it for the kind of fine-grained control I'd like&mdash;but for this kind of prototype work, it was fantastically quick.
+
+As a bonus to JQuery, I took advantage of a few of the amazingly cool helper functions offered by [Underscore.js](http://documentcloud.github.com/underscore/).
+
+For the vector images that make up the canvas, I used [Raphael.js](http://raphaeljs.com/). There were a few quirks I ran into with this library, but they all boiled down to me not fully understanding SVGs, rather than Raphael not acting like a well-versed user would expect. So nuts to SVGs, I guess.
+
+Finally I have to give major thanks to [Xueqiao Xu](https://github.com/qiao/) and his excellent [PathFinding.js](https://github.com/qiao/PathFinding.js) library which acted as a fantastic starting point for me. I've since moved a pretty good distance away from his work, but still. Good stuff.
+
+-----
+
+### License ###
+
+This project is released under the [MIT License](http://www.opensource.org/licenses/mit-license.php).
+
+-----
+
+#### To Dos ####
+
+ * Implement & test on Firefox, Opera, Safari.... IE?
+ * Add `Turn off Animations` check box
---
+++ dev/js/GoL.coffee.js
@@ -0,0 +1,557 @@
+(function() {
+  var GoL;
+
+  GoL = function(canvas_element, width, height) {
+    var anim_duration, cell, dying_color, gol, max_zoom, min_zoom, mouse_left, mouse_right, neighbor_set, node_size, raising_color, scroll_threshold, stroke_opacity, zoom_scalar, _ref;
+    var _this = this;
+    raising_color = "#005500";
+    dying_color = "#550000";
+    stroke_opacity = 0.2;
+    anim_duration = 125;
+    gol = {};
+    gol.model = {};
+    gol.view = {};
+    gol.ctrl = {};
+    cell = function(born) {
+      return {
+        birthday: born,
+        deathday: -1,
+        state: "born",
+        neighbors: 0
+      };
+    };
+    neighbor_set = [
+      {
+        x: 1,
+        y: 0
+      }, {
+        x: 1,
+        y: -1
+      }, {
+        x: 0,
+        y: -1
+      }, {
+        x: -1,
+        y: -1
+      }, {
+        x: -1,
+        y: 0
+      }, {
+        x: -1,
+        y: 1
+      }, {
+        x: 0,
+        y: 1
+      }, {
+        x: 1,
+        y: 1
+      }
+    ];
+    gol.model.current_step = 0;
+    gol.model.cell_count = 0;
+    gol.model.live_cells = {};
+    gol.model.fresh_cells = [];
+    gol.model.raiseCell = function(x, y) {
+      var _base, _ref;
+      if ((_ref = (_base = this.live_cells)[x]) == null) _base[x] = {};
+      this.live_cells[x][y] = cell(this.current_step);
+      this.fresh_cells.push([x, y]);
+      this.cell_count += 1;
+      gol.view.addRect(x, y);
+      return;
+    };
+    gol.model.killCell = function(x, y) {
+      delete this.live_cells[x][y];
+      if ($.isEmptyObject(this.live_cells[x])) delete this.live_cells[x];
+      this.cell_count -= 1;
+      gol.view.removeRect(x, y);
+      return;
+    };
+    gol.model.isAliveAt = function(x, y) {
+      var _ref;
+      return ((_ref = this.live_cells[x]) != null ? _ref[y] : void 0) != null;
+    };
+    gol.model.step = function() {
+      var cell, cell_col, neighbor, neighbor_count, seeds, x, x_string, y, y_string, _base, _i, _len, _name, _name2, _ref, _ref2, _ref3, _ref4, _ref5;
+      this.current_step += 1;
+      seeds = {};
+      _ref = this.live_cells;
+      for (x_string in _ref) {
+        cell_col = _ref[x_string];
+        for (y_string in cell_col) {
+          cell = cell_col[y_string];
+          x = parseInt(x_string);
+          y = parseInt(y_string);
+          for (_i = 0, _len = neighbor_set.length; _i < _len; _i++) {
+            neighbor = neighbor_set[_i];
+            if (((_ref2 = this.live_cells[x + neighbor.x]) != null ? _ref2[y + neighbor.y] : void 0) != null) {
+              this.live_cells[x + neighbor.x][y + neighbor.y].neighbors += 1;
+            } else {
+              if ((_ref3 = seeds[_name = x + neighbor.x]) == null) {
+                seeds[_name] = {};
+              }
+              if ((_ref4 = (_base = seeds[x + neighbor.x])[_name2 = y + neighbor.y]) == null) {
+                _base[_name2] = 0;
+              }
+              seeds[x + neighbor.x][y + neighbor.y] += 1;
+            }
+          }
+        }
+      }
+      _ref5 = this.live_cells;
+      for (x_string in _ref5) {
+        cell_col = _ref5[x_string];
+        for (y_string in cell_col) {
+          cell = cell_col[y_string];
+          neighbor_count = cell.neighbors;
+          if (neighbor_count !== 2 && neighbor_count !== 3) {
+            x = parseInt(x_string);
+            y = parseInt(y_string);
+            this.killCell(x, y);
+          } else {
+            cell.neighbors = 0;
+          }
+        }
+      }
+      for (x_string in seeds) {
+        cell_col = seeds[x_string];
+        for (y_string in cell_col) {
+          neighbor_count = cell_col[y_string];
+          if (neighbor_count === 3) {
+            x = parseInt(x_string);
+            y = parseInt(y_string);
+            this.raiseCell(x, y);
+          }
+        }
+      }
+      return;
+    };
+    gol.model.reset = function() {
+      gol.model.current_step = 0;
+      gol.model.cell_count = 0;
+      return gol.model.live_cells = {};
+    };
+    node_size = 15;
+    min_zoom = 0.2;
+    max_zoom = 10;
+    zoom_scalar = function() {
+      return gol.view.current_zoom * 0.0025;
+    };
+    gol.view.paper = Raphael(canvas_element.slice(1));
+    gol.view.rects = {};
+    gol.view.width = 0;
+    gol.view.height = 0;
+    gol.view.current_zoom = 1;
+    gol.view.scaled_node_size = node_size;
+    gol.view.zoom_offset = {
+      x: 0.5,
+      y: 0.5
+    };
+    gol.view.offset = {
+      x: 0,
+      y: 0
+    };
+    gol.view.grid_offset = {
+      x: 0,
+      y: 0
+    };
+    gol.view.px_offset = {
+      x: 0,
+      y: 0
+    };
+    gol.view.resizeCanvas = function(width, height) {
+      this.width = width;
+      this.height = height;
+      this.paper.setSize(this.width, this.height);
+      this.drawGrid();
+      return;
+    };
+    gol.view.setZoomOffset = function(page_x, page_y) {
+      this.zoom_offset = {
+        x: page_x / this.width,
+        y: page_y / this.height
+      };
+      return;
+    };
+    gol.view.zoom = function(delta) {
+      var current_zoom, old_zoom;
+      old_zoom = this.current_zoom;
+      this.current_zoom += zoom_scalar() * delta;
+      if (this.current_zoom > max_zoom) {
+        this.current_zoom = max_zoom;
+      } else if (this.current_zoom < min_zoom) {
+        this.current_zoom = min_zoom;
+      }
+      if (old_zoom !== this.current_zoom) {
+        this.scaled_node_size = node_size * this.current_zoom;
+        current_zoom = this.current_zoom;
+        this.moveOffset(-(this.width / old_zoom - this.width / current_zoom) * this.zoom_offset.x, -(this.height / old_zoom - this.height / current_zoom) * this.zoom_offset.y, false);
+      }
+      return;
+    };
+    gol.view.moveOffset = function(delta_x, delta_y, scale) {
+      var _this = this;
+      if (scale == null) scale = true;
+      if (scale) {
+        this.offset.x += delta_x * (1 / this.current_zoom);
+        this.offset.y += delta_y * (1 / this.current_zoom);
+      } else {
+        this.offset.x += delta_x;
+        this.offset.y += delta_y;
+      }
+      this.grid_offset = {
+        x: Math.floor((this.offset.x * this.current_zoom) / this.scaled_node_size),
+        y: Math.floor((this.offset.y * this.current_zoom) / this.scaled_node_size)
+      };
+      this.px_offset = {
+        x: (this.offset.x * this.current_zoom) % this.scaled_node_size,
+        y: (this.offset.y * this.current_zoom) % this.scaled_node_size
+      };
+      if (this.px_offset.x < 0) this.px_offset.x += this.scaled_node_size;
+      if (this.px_offset.y < 0) this.px_offset.y += this.scaled_node_size;
+      _.defer(function() {
+        return _this.drawGrid();
+      });
+      return;
+    };
+    gol.view.drawGrid = _.throttle((function() {
+      var i, j, node_cols, node_rows, x, y, _ref, _ref2, _ref3, _ref4;
+      this.paper.clear();
+      this.rects = {};
+      node_cols = 1 + Math.ceil(this.width / this.scaled_node_size);
+      node_rows = 1 + Math.ceil(this.height / this.scaled_node_size);
+      for (i = 0; 0 <= node_cols ? i <= node_cols : i >= node_cols; 0 <= node_cols ? i++ : i--) {
+        this.paper.path(("M" + (i * this.scaled_node_size + this.px_offset.x) + ",0") + ("L" + (i * this.scaled_node_size + this.px_offset.x) + "," + this.height)).attr({
+          "stroke-opacity": .2
+        });
+      }
+      for (j = 0; 0 <= node_rows ? j <= node_rows : j >= node_rows; 0 <= node_rows ? j++ : j--) {
+        this.paper.path(("M0," + (j * this.scaled_node_size + this.px_offset.y)) + ("L" + this.width + "," + (j * this.scaled_node_size + this.px_offset.y))).attr({
+          "stroke-opacity": .2
+        });
+      }
+      for (x = _ref = 0 - this.grid_offset.x, _ref2 = node_cols - this.grid_offset.x; _ref <= _ref2 ? x < _ref2 : x > _ref2; _ref <= _ref2 ? x++ : x--) {
+        if (gol.model.live_cells[x] != null) {
+          for (y = _ref3 = 0 - this.grid_offset.y, _ref4 = node_rows - this.grid_offset.y; _ref3 <= _ref4 ? y < _ref4 : y > _ref4; _ref3 <= _ref4 ? y++ : y--) {
+            if (gol.model.live_cells[x][y] != null) this.drawRect(x, y);
+          }
+        }
+      }
+      return;
+    }), 3);
+    gol.view.addRect = function(x, y) {
+      var grid_x, grid_y, _base, _base2, _ref, _ref2;
+      grid_x = x + this.grid_offset.x;
+      grid_y = y + this.grid_offset.y;
+      if ((_ref = (_base = this.rects)[grid_x]) == null) _base[grid_x] = {};
+      if ((_ref2 = (_base2 = this.rects[grid_x])[grid_y]) == null) {
+        _base2[grid_y] = this.paper.rect(grid_x * this.scaled_node_size + this.px_offset.x, grid_y * this.scaled_node_size + this.px_offset.y, this.scaled_node_size, this.scaled_node_size, this.scaled_node_size / 5).attr({
+          "fill": raising_color,
+          "stroke-opacity": 0.2,
+          "transform": "S0.0",
+          "opacity": 0
+        }).animate({
+          "transform": "S1.0",
+          "opacity": 1,
+          "stroke-width": 1
+        }, anim_duration);
+      }
+      return;
+    };
+    gol.view.drawRect = function(x, y) {
+      var grid_x, grid_y, _base, _base2, _ref, _ref2;
+      grid_x = x + this.grid_offset.x;
+      grid_y = y + this.grid_offset.y;
+      if ((_ref = (_base = this.rects)[grid_x]) == null) _base[grid_x] = {};
+      return (_ref2 = (_base2 = this.rects[grid_x])[grid_y]) != null ? _ref2 : _base2[grid_y] = this.paper.rect(grid_x * this.scaled_node_size + this.px_offset.x, grid_y * this.scaled_node_size + this.px_offset.y, this.scaled_node_size, this.scaled_node_size, this.scaled_node_size / 5).attr({
+        "fill": raising_color,
+        "stroke-opacity": 0.2,
+        "stroke-width": 1
+      });
+    };
+    gol.view.removeRect = function(x, y) {
+      var grid_x, grid_y, _ref;
+      grid_x = x + this.grid_offset.x;
+      grid_y = y + this.grid_offset.y;
+      if (((_ref = this.rects[grid_x]) != null ? _ref[grid_y] : void 0) != null) {
+        this.rects[grid_x][grid_y].attr({
+          "fill": dying_color
+        }).animate({
+          "opacity": 0,
+          "transform": "S0.0"
+        }, anim_duration);
+        _.delay((function(rect) {
+          if (rect.node.parentNode != null) rect.remove();
+          return;
+        }), anim_duration, this.rects[grid_x][grid_y]);
+        delete this.rects[grid_x][grid_y];
+        if ($.isEmptyObject(this.rects[grid_x])) delete this.rects[grid_x];
+      }
+      return;
+    };
+    gol.view.pageToGrid = function(page_x, page_y) {
+      return {
+        x: Math.floor((page_x - this.px_offset.x) / this.scaled_node_size),
+        y: Math.floor((page_y - this.px_offset.y) / this.scaled_node_size)
+      };
+    };
+    gol.view.pageToAbs = function(page_x, page_y) {
+      return {
+        x: Math.floor((page_x - this.px_offset.x) / this.scaled_node_size) - this.grid_offset.x,
+        y: Math.floor((page_y - this.px_offset.y) / this.scaled_node_size) - this.grid_offset.y
+      };
+    };
+    scroll_threshold = 5;
+    _ref = [0, 2], mouse_left = _ref[0], mouse_right = _ref[1];
+    gol.ctrl.active = [false, false];
+    gol.ctrl.drag_start = {
+      x: 0,
+      y: 0
+    };
+    gol.ctrl.page_last = {
+      x: 0,
+      y: 0
+    };
+    gol.ctrl.abs_last = {
+      x: 0,
+      y: 0
+    };
+    gol.ctrl.moving = false;
+    gol.ctrl.hz = 4;
+    gol.ctrl.running = void 0;
+    gol.ctrl.resolveMousedown = function(page_x, page_y, button) {
+      var abs;
+      this.active[button] = true;
+      this.page_last = {
+        x: page_x,
+        y: page_y
+      };
+      abs = gol.view.pageToAbs(page_x, page_y);
+      if (this.active[mouse_left] && this.active[mouse_right]) {
+        gol.view.setZoomOffset(page_x, page_y);
+      } else if (this.active[mouse_left]) {
+        if (gol.model.isAliveAt(abs.x, abs.y)) {
+          gol.model.killCell(abs.x, abs.y);
+          this.active[mouse_left] = "kill";
+        } else {
+          gol.model.raiseCell(abs.x, abs.y);
+          this.active[mouse_left] = "raise";
+        }
+        this.abs_last = {
+          x: abs.x,
+          y: abs.y
+        };
+      }
+      return;
+    };
+    gol.ctrl.resolveMouseup = function(button) {
+      var _this = this;
+      this.active[button] = false;
+      if (!this.active[mouse_left] && !this.active[mouse_right]) {
+        this.moving = false;
+        _.defer(function() {
+          return $("#draw_space").attr("oncontextmenu", " ");
+        });
+      }
+      return;
+    };
+    gol.ctrl.resolveMousemove = function(page_x, page_y) {
+      var abs, delta_x, delta_y;
+      delta_x = this.page_last.x - page_x;
+      delta_y = this.page_last.y - page_y;
+      if (!this.moving && Math.abs(delta_x) + Math.abs(delta_y) > scroll_threshold) {
+        this.moving = true;
+        $("#draw_space").attr("oncontextmenu", "return false");
+        this.drag_start = {
+          x: this.page_last.x,
+          y: this.page_last.y
+        };
+      }
+      if (this.moving) {
+        if (this.active[mouse_left] && this.active[mouse_right]) {
+          gol.view.zoom(delta_y);
+        } else if (this.active[mouse_right]) {
+          gol.view.moveOffset(-delta_x, -delta_y);
+        } else {
+          abs = gol.view.pageToAbs(page_x, page_y);
+          if (abs.x !== this.abs_last.x || abs.y !== this.abs_last.y) {
+            this.abs_last = {
+              x: abs.x,
+              y: abs.y
+            };
+            if (this.active[mouse_left] === "raise") {
+              gol.model.raiseCell(abs.x, abs.y);
+            } else if (gol.model.isAliveAt(abs.x, abs.y)) {
+              gol.model.killCell(abs.x, abs.y);
+            }
+          }
+        }
+        this.page_last.x = page_x;
+        this.page_last.y = page_y;
+      }
+      return;
+    };
+    gol.ctrl.setHz = function(hz) {
+      var _this = this;
+      this.hz = hz;
+      anim_duration = 1000 / (2 * gol.ctrl.hz);
+      if (anim_duration > 200) {
+        anim_duration = 200;
+      } else if (anim_duration < 30) {
+        anim_duration = 0;
+      }
+      if (this.running != null) {
+        clearTimeout(this.running);
+        this.running = setInterval((function() {
+          return gol.model.step();
+        }), 1000 / this.hz);
+      }
+      return;
+    };
+    gol.ctrl.start = function() {
+      var _ref2;
+      var _this = this;
+      if ((_ref2 = this.running) == null) {
+        this.running = setInterval((function() {
+          return gol.model.step();
+        }), 1000 / this.hz);
+      }
+      return;
+    };
+    gol.ctrl.stop = function() {
+      clearTimeout(this.running);
+      delete this.running;
+      return;
+    };
+    gol.ctrl.reset = function() {
+      return this.stop();
+    };
+    (function() {
+      var $raphael;
+      var _this = this;
+      $raphael = $(canvas_element);
+      $raphael.on("mousedown", function(event) {
+        gol.ctrl.resolveMousedown(event.pageX, event.pageY, event.button);
+        $raphael.on("mousemove", function(event) {
+          gol.ctrl.resolveMousemove(event.pageX, event.pageY);
+          return false;
+        });
+        return false;
+      });
+      $(window).on("mouseup", function(event) {
+        gol.ctrl.resolveMouseup(event.button);
+        if (!gol.ctrl.active[mouse_left] && !gol.ctrl.active[mouse_right]) {
+          $raphael.off("mousemove");
+        }
+        return false;
+      });
+      $(window).on("resize", _.debounce((function() {
+        gol.view.resizeCanvas($(window).width(), $(window).height());
+        return;
+      }), 90));
+      return true;
+    })();
+    gol.step = function() {
+      gol.model.step();
+      return;
+    };
+    gol.start = function() {
+      return gol.ctrl.start();
+    };
+    gol.stop = function() {
+      return gol.ctrl.stop();
+    };
+    gol.setHz = function(hz) {
+      return gol.ctrl.setHz(hz);
+    };
+    gol.reset = function() {
+      gol.ctrl.reset();
+      gol.model.reset();
+      gol.view.drawGrid();
+      return;
+    };
+    gol.view.resizeCanvas(width, height);
+    return gol;
+  };
+
+  $(function() {
+    var max_hz, min_hz, play;
+    window.game_of_life = GoL("#draw_space", $(window).width(), $(window).height());
+    $(".movable_pane").draggable();
+    $("#hide").on("click", function(event) {
+      $("#help_pane").slideUp();
+      return;
+    });
+    $("#step_reset_set").buttonset();
+    play = false;
+    $("#play").button();
+    $("#play").on("click", function(event) {
+      if (!play) {
+        play = true;
+        game_of_life.start();
+      } else {
+        play = false;
+        game_of_life.stop();
+      }
+      return;
+    });
+    $("#step").button();
+    $("#step").on("click", function(event) {
+      game_of_life.step();
+      return;
+    });
+    $("#reset").button();
+    $("#reset").on("click", function(event) {
+      game_of_life.reset();
+      if (play) $("#play").click();
+      return;
+    });
+    min_hz = 1;
+    max_hz = 50;
+    $("#hz_slider").slider({
+      min: min_hz,
+      max: max_hz,
+      value: 4,
+      step: 1,
+      slide: function(event, ui) {
+        $("#hz").val(ui.value);
+        game_of_life.setHz(ui.value);
+        return;
+      },
+      change: function(event, ui) {
+        $("#hz").val(ui.value);
+        game_of_life.setHz(ui.value);
+        return;
+      }
+    });
+    $("#hz").on("change", function(event) {
+      var val;
+      val = $("#hz").val();
+      if (isNaN(val)) {
+        ui.val("NaN");
+        return;
+      }
+      val = parseInt(val);
+      if (val > max_hz) {
+        val = max_hz;
+      } else if (val < min_hz) {
+        val = min_hz;
+      }
+      $("#hz").val(val);
+      $("#hz_slider").slider("value", val);
+      return;
+    });
+    $("div.tab_content:first").show();
+    $("ul.tab_menu li").on("click", function(event) {
+      var activeTab;
+      $("ul.tab_menu li").removeClass("open");
+      $(".tab_content").hide();
+      $(this).addClass("open");
+      activeTab = $(this).find("a").attr("href");
+      $(activeTab).fadeIn();
+      return false;
+    });
+    return;
+  });
+
+}).call(this);
